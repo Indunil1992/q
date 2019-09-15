b6dcac79ad10aec9419b8d6fdf1f73327428c4c7
Indunil1992 <indunil@adroitlogic.com>
2019-09-15 09:19:07 +0530

    q

--- q/lambda.js
+++ q/lambda.js
@@ -1,4 +1,20 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+    sns.subscribe({
+        Protocol: 'https',
+        Endpoint: 'https://052p9kjn18.execute-api.us-east-1.amazonaws.com/test11/indunil/qa	',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            console.log(data);
+            // your code goes here
+        })
+        .catch(err => {
+            console.log(err);
+            // error handling goes here
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
